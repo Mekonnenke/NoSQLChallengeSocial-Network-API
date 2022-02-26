2eef91d6c2873fd0572430c35983bff9706e23f7
Mona Lisa <mekotigi@gmail.com>
2022-02-25 22:47:42 -0500

    user routes

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/NoSQLChallengeSocial-Network-API', {
+  useNewUrlParser: true,
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
@@ -16,5 +17,8 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hun
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
 
+app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
 
 // https://github.com/walidhkhan/noSQL-network/tree/main/models
